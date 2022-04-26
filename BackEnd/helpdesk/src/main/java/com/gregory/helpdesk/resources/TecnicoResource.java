6cc18d75636b4c50c8f5dfd0ba659e5e424cf285
Gregory Lacerda <gregorylacerda@hotmail.com>
2022-04-26 16:55:35 -0300

    Endpoint create para Tecnico

--- BackEnd/helpdesk/src/main/java/com/gregory/helpdesk/resources/TecnicoResource.java
+++ BackEnd/helpdesk/src/main/java/com/gregory/helpdesk/resources/TecnicoResource.java
@@ -1,14 +1,20 @@
 package com.gregory.helpdesk.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import javax.servlet.ServletSecurityElement;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gregory.helpdesk.domain.Tecnico;
 import com.gregory.helpdesk.domain.dtos.TecnicoDTO;
@@ -36,5 +42,14 @@ public class TecnicoResource {
 		List<TecnicoDTO> listDTO = list.stream().map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList());
 		
 		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@PostMapping
+	public ResponseEntity<TecnicoDTO> create(@RequestBody TecnicoDTO objDTO){
+		
+		Tecnico newObj = service.create(objDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 }
--- BackEnd/helpdesk/src/main/java/com/gregory/helpdesk/services/TecnicoService.java
+++ BackEnd/helpdesk/src/main/java/com/gregory/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gregory.helpdesk.domain.Tecnico;
+import com.gregory.helpdesk.domain.dtos.TecnicoDTO;
 import com.gregory.helpdesk.repositories.TecnicoRepository;
 import com.gregory.helpdesk.services.exceptions.ObjectNotFoundException;
 
@@ -25,6 +26,13 @@ public class TecnicoService {
 
 	public List<Tecnico> findAll() {
 		return repository.findAll();
+	}
+
+	public Tecnico create(TecnicoDTO objDTO) {
+		objDTO.setId(null);
+		Tecnico newObj = new Tecnico(objDTO);
+		
+		return repository.save(newObj);
 	}
 	
 }
